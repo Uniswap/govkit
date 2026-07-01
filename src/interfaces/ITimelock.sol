5bd0b41eb775795f03f15994e9b39a612554605b
jtriley <jt.riley@uniswap.org>
2026-07-01 09:06:31 -0700

    add timelock interface

---
+++ src/interfaces/ITimelock.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.8.0;
+
+interface ITimelock {
+
+    // Events
+    event CancelTransaction(bytes32 indexed txHash, address indexed target, uint256 value, string signature, bytes data, uint256 eta);
+    event ExecuteTransaction(bytes32 indexed txHash, address indexed target, uint256 value, string signature, bytes data, uint256 eta);
+    event NewAdmin(address indexed newAdmin);
+    event NewDelay(uint256 indexed newDelay);
+    event NewPendingAdmin(address indexed newPendingAdmin);
+    event QueueTransaction(bytes32 indexed txHash, address indexed target, uint256 value, string signature, bytes data, uint256 eta);
+
+    // Functions
+    function GRACE_PERIOD() external view returns (uint256);
+    function MAXIMUM_DELAY() external view returns (uint256);
+    function MINIMUM_DELAY() external view returns (uint256);
+    function acceptAdmin() external;
+    function admin() external view returns (address);
+    function cancelTransaction(address target, uint256 value, string memory signature, bytes memory data, uint256 eta) external;
+    function delay() external view returns (uint256);
+    function executeTransaction(address target, uint256 value, string memory signature, bytes memory data, uint256 eta) external payable returns (bytes memory);
+    function pendingAdmin() external view returns (address);
+    function queueTransaction(address target, uint256 value, string memory signature, bytes memory data, uint256 eta) external returns (bytes32);
+    function queuedTransactions(bytes32) external view returns (bool);
+    function setDelay(uint256 delay_) external;
+    function setPendingAdmin(address pendingAdmin_) external;
+}
