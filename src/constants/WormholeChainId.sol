d252d39e71a8c1580e8460b0e65c2fee96ba984f
jtriley <jt.riley@uniswap.org>
2026-07-06 10:29:24 -0700

    update wormhole chain ids

--- src/constants/WormholeChainId.sol
+++ src/constants/WormholeChainId.sol
@@ -8,79 +8,70 @@ import {ChainId} from "./ChainId.sol";
 /// @dev This is Wormhole's chain ID system for arbitrary blockchains. We
 ///      implement this library to fully encapsulate this system so proposal
 ///      writers only need to focus on the canonical chain ID system.
+/// @dev Note that this is not comprehensive, it only covers chains where we use
+///      Wormhole actively.
 library WormholeChainId {
     /// @dev Thrown when an EIP-155 chain ID does not map to a Wormhole chain ID.
-    error UnknownWormholeChainId(uint256 chainId);
+    error UnsupportedChainId(uint256 chainId);
 
     /// @dev Thrown when a Wormhole chain ID does not map to an EIP-155 chain ID.
-    error UnknownChainId(uint16 wormholeChainId);
+    error UnsupportedWormholeChainId(uint16 wormholeChainId);
 
-    uint16 internal constant Arbitrum = 23;
     uint16 internal constant Avalanche = 6;
-    uint16 internal constant Base = 30;
     uint16 internal constant BNBChain = 4;
-    uint16 internal constant Celo = 14;
     uint16 internal constant Ethereum = 2;
-    uint16 internal constant Optimism = 24;
-    uint16 internal constant Polygon = 5;
-    uint16 internal constant UniChain = 44;
-    uint16 internal constant WorldChain = 45;
+    uint16 internal constant MegaEth = 64;
+    uint16 internal constant Monad = 48;
+    uint16 internal constant RootStock = 33;
+    uint16 internal constant Tempo = 68;
 
     /// @dev Maps an EIP-155 chain ID to a Wormhole chain ID.
     /// @param chainId EIP-155 chain ID.
     /// @return Wormhole chain ID.
-    function chainIdToWormholeChainId(uint256 chainId) internal pure returns (uint16) {
-        if (chainId == ChainId.Arbitrum) {
-            return Arbitrum;
-        } else if (chainId == ChainId.Avalanche) {
+    function chainIdToWormholeChainId(
+        uint256 chainId
+    ) internal pure returns (uint16) {
+        if (chainId == ChainId.Avalanche) {
             return Avalanche;
-        } else if (chainId == ChainId.Base) {
-            return Base;
         } else if (chainId == ChainId.BNBChain) {
             return BNBChain;
-        } else if (chainId == ChainId.Celo) {
-            return Celo;
         } else if (chainId == ChainId.Ethereum) {
             return Ethereum;
-        } else if (chainId == ChainId.Optimism) {
-            return Optimism;
-        } else if (chainId == ChainId.Polygon) {
-            return Polygon;
-        } else if (chainId == ChainId.UniChain) {
-            return UniChain;
-        } else if (chainId == ChainId.WorldChain) {
-            return WorldChain;
+        } else if (chainId == ChainId.MegaEth) {
+            return MegaEth;
+        } else if (chainId == ChainId.Monad) {
+            return Monad;
+        } else if (chainId == ChainId.RootStock) {
+            return RootStock;
+        } else if (chainId == ChainId.Tempo) {
+            return Tempo;
         }
 
-        revert UnknownWormholeChainId(chainId);
+        revert UnsupportedChainId(chainId);
     }
 
-    /// @dev Maps an EIP-155 chain ID to a Wormhole chain ID.
+    /// @dev Maps a Wormhole chain ID to an EIP-155 chain ID.
     /// @param wormholeChainId Wormhole chain ID.
     /// @return EIP-155 chain ID.
-    function wormholeChainIdtoChainId(uint16 wormholeChainId) internal pure returns (uint256) {
-        if (wormholeChainId == Arbitrum) {
-            return ChainId.Arbitrum;
-        } else if (wormholeChainId == Avalanche) {
+    function wormholeChainIdtoChainId(
+        uint16 wormholeChainId
+    ) internal pure returns (uint256) {
+        if (wormholeChainId == Avalanche) {
             return ChainId.Avalanche;
-        } else if (wormholeChainId == Base) {
-            return ChainId.Base;
         } else if (wormholeChainId == BNBChain) {
             return ChainId.BNBChain;
-        } else if (wormholeChainId == Celo) {
-            return ChainId.Celo;
         } else if (wormholeChainId == Ethereum) {
             return ChainId.Ethereum;
-        } else if (wormholeChainId == Optimism) {
-            return ChainId.Optimism;
-        } else if (wormholeChainId == Polygon) {
-            return ChainId.Polygon;
-        } else if (wormholeChainId == UniChain) {
-            return ChainId.UniChain;
-        } else if (wormholeChainId == WorldChain) {
-            return ChainId.WorldChain;
+        } else if (wormholeChainId == MegaEth) {
+            return ChainId.MegaEth;
+        } else if (wormholeChainId == Monad) {
+            return ChainId.Monad;
+        } else if (wormholeChainId == RootStock) {
+            return ChainId.RootStock;
+        } else if (wormholeChainId == Tempo) {
+            return ChainId.Tempo;
         }
 
-        revert UnknownChainId(wormholeChainId);
+        revert UnsupportedWormholeChainId(wormholeChainId);
     }
 }
